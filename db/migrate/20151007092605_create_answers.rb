455f33e5468728ad6749dd05dfd0d6728ee243be
Ming Xiang <mingxiangchan@gmail.com>
2015-10-07 09:52:46 +0800

    completed answers migration

--- db/migrate/20151007092605_create_answers.rb
+++ db/migrate/20151007092605_create_answers.rb
@@ -1,4 +1,11 @@
 class CreateAnswers < ActiveRecord::Migration
 	def change
+	  	create_table :answers do |t|        
+	      t.references :users, index: true
+	      t.references :questions, index: true
+	      t.string :description
+
+	      t.timestamps null: false
+	    end
 	end
 end
