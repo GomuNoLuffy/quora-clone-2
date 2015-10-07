c049e5e4ff2ed5390b9cd49a944b92640a31142d
Ming Xiang <mingxiangchan@gmail.com>
2015-10-07 09:49:47 +0800

    created questions migration

--- db/migrate/20151007092559_create_questions.rb
+++ db/migrate/20151007092559_create_questions.rb
@@ -1,4 +1,10 @@
 class CreateQuestions < ActiveRecord::Migration
 	def change
+	  	create_table :questions do |t|        
+	      t.references :users, index: true
+	      t.string :description
+
+	      t.timestamps null: false
+	    end
 	end
 end
