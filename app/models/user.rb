603509d4511469da078e2b94c217867e68983f2a
Ming Xiang <mingxiangchan@gmail.com>
2015-10-08 10:53:48 +0800

    added username for Users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
 	has_many :questions
 	has_many :answers
+    validates :username, uniqueness: true, length: { maximum: 64 }
     validates :email, uniqueness: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
 
   def self.authenticate(email , password)
